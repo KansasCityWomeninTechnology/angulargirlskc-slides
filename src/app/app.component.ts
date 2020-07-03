2369d58136666b8fc99383e00d40c6944d4a9b7b
Alisa <7586237+alisaduncan@users.noreply.github.com>
2020-07-03 16:37:32 -0500

    Cleanup, remove Facebook, and update schedule (#2)
    * fix social and update schedule
    
    * remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     new ComponentPortal(SocialComponent)
   ];
 
-  public slide$: Observable<ComponentPortal<any>>; // Observable<number>;
+  public slide$: Observable<ComponentPortal<any>>;
 
 
   constructor() {}
