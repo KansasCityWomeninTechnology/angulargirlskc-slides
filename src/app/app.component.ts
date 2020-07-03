24a66caed994370fa5c18b3a4ca38ef0858774fb
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2020-07-03 16:36:29 -0500

    remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { ComponentPortal } from '@angular/cdk/portal';
 import { LinksComponent } from './links/links.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { SocialComponent } from './social/social.component';
-import {interval, Observable, of} from 'rxjs';
+import { interval, Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
 @Component({
