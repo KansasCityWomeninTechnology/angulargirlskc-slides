44fe1e1cc3acc1eb818a652a5d90eeabb09b44fb
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2020-07-17 18:11:13 -0500

    fix slide ordering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { SocialComponent } from './social/social.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
 import { RafflePrizesComponent } from './raffle-prizes/raffle-prizes.component';
-import {interval, Observable, of} from 'rxjs';
+import { interval, Observable, of } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
 @Component({
@@ -18,9 +18,9 @@ export class AppComponent implements OnInit {
   private slidesToDisplay: ComponentPortal<any>[] = [
     new ComponentPortal(LinksComponent),
     new ComponentPortal(ScheduleComponent),
-    new ComponentPortal(SocialComponent),
     new ComponentPortal(SponsorsComponent),
-    new ComponentPortal(RafflePrizesComponent)
+    new ComponentPortal(RafflePrizesComponent),
+    new ComponentPortal(SocialComponent)
   ];
 
   public slide$: Observable<ComponentPortal<any>>;
@@ -30,8 +30,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.slide$ = interval(15000).pipe(
-      startWith(2),
-      map(num => this.slidesToDisplay[num % 4])
+      startWith(4),
+      map(num => this.slidesToDisplay[num % 5])
     );
   }
 }
