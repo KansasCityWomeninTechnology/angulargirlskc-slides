a69f3931644a15004a789ec67bd388e06575ec40
Alisa <7586237+alisaduncan@users.noreply.github.com>
2020-07-17 18:13:10 -0500

    Add raffle prize slide and update schedule (#7)
    * Add raffle prizes and update schedule
    
    * fix slide ordering

---
+++ src/app/raffle-prizes/raffle-prizes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raffle-prizes',
+  templateUrl: './raffle-prizes.component.html',
+  styleUrls: ['./raffle-prizes.component.scss']
+})
+export class RafflePrizesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
